9fd96dcf9e3c3a47420ef4ca26f5454e914609f7
Matthewokimute <ego.okiemute@gmail.com>
2024-06-03 00:00:00 +0100

    added contents and social links

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,53 +1,63 @@
-'use strict';
+async function handleFormSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
 
-const form = document.getElementById('form');
-const result = document.getElementById('result');
-const modal = document.getElementById('modal');
-const closeButton = document.querySelector('.close-button');
-
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const object = Object.fromEntries(formData);
-    const json = JSON.stringify(object);
-    result.innerHTML = "Please wait...";
-    modal.style.display = "block";
-
-    fetch('https://api.web3forms.com/submit', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: json
-    })
-        .then(async (response) => {
-            let json = await response.json();
-            if (response.status == 200) {
-                result.innerHTML = json.message;
-            } else {
-                console.log(response);
-                result.innerHTML = json.message;
-            }
-        })
-        .catch(error => {
-            console.log(error);
-            result.innerHTML = "Something went wrong!";
-        })
-        .then(function () {
-            form.reset();
-            setTimeout(() => {
-                modal.style.display = "none";
-            }, 3000);
+    try {
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify(Object.fromEntries(formData)),
         });
-});
 
-closeButton.addEventListener('click', () => {
-    modal.style.display = "none";
-});
+        const successMessage = document.getElementById("success-message");
+        const errorMessage = document.getElementById("error-message");
+        const backdrop = document.createElement("div");
+        backdrop.classList.add("backdrop");
 
-window.addEventListener('click', (event) => {
-    if (event.target == modal) {
-        modal.style.display = "none";
+        document.body.appendChild(backdrop);
+
+        if (response.status === 200) {
+            successMessage.textContent = "Thank you for your message. We will reply to you shortly!";
+            successMessage.classList.remove("d-none");
+            successMessage.classList.add("d-block");
+            errorMessage.classList.add("d-none");
+            document.getElementById("contact-form").reset();
+        } else {
+            console.log(response);
+            errorMessage.textContent = "Something went wrong! Please try again";
+            errorMessage.classList.remove("d-none");
+            errorMessage.classList.add("d-block");
+            successMessage.classList.add("d-none");
+        }
+
+        backdrop.classList.add("d-block");
+
+        // Remove messages and backdrop after 3 seconds
+        setTimeout(() => {
+            successMessage.classList.remove("d-block");
+            successMessage.classList.add("d-none");
+            errorMessage.classList.remove("d-block");
+            errorMessage.classList.add("d-none");
+            backdrop.classList.remove("d-block");
+            document.body.removeChild(backdrop);
+        }, 3000);
+    } catch (error) {
+        console.log(error)
+        const errorMessage = document.getElementById("error-message");
+        errorMessage.textContent = "An error occurred while submitting the form.";
+        errorMessage.classList.remove("d-none");
+        errorMessage.classList.add("d-block");
+        backdrop.classList.add("d-block");
+
+        // Remove messages and backdrop after 3 seconds
+        setTimeout(() => {
+            errorMessage.classList.remove("d-block");
+            errorMessage.classList.add("d-none");
+            backdrop.classList.remove("d-block");
+            document.body.removeChild(backdrop);
+        }, 3000);
     }
-});
+}
